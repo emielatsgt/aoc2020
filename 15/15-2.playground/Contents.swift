227a8d3691de34f509cc780d8e26a6e8b6032fc8
Emiel Lensink <emiel.lensink@sogeti.com>
2020-12-15 09:54:36 +0100

    Somewhat faster solution for day 15, part 2

--- 15/15-2.playground/Contents.swift
+++ 15/15-2.playground/Contents.swift
@@ -8,31 +8,42 @@ let numbers = input
     .components(separatedBy: ",")
     .map { Int($0)! }
 
-print(numbers)
-
-var memory: [Int: [Int]] = [:]
+var memory: [Int: (Int, Int)] = [:]
 
 for (index, number) in numbers.enumerated() {
-    memory[number] = [index + 1]
+    memory[number] = (index + 1, 0)
 }
 
 var turn = numbers.count + 1
 var lastNumber = numbers.last!
 
-while turn < 30000001 {
-    let spoken = memory[lastNumber]!
-    
-    if spoken.count == 1 {
-        lastNumber = 0
-    } else {
-        lastNumber = spoken[0] - spoken[1]
-    }
-
-    let temp = memory[lastNumber] ?? []
-    memory[lastNumber] = Array(([turn] + temp).prefix(2))
-
-    turn += 1
-    if turn % 10000 == 0 { print("At \(turn)") }
+func printTimeElapsedWhenRunningCode(title:String, operation:()->()) {
+    let startTime = CFAbsoluteTimeGetCurrent()
+        operation()
+    let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+    print("Time elapsed for \(title): \(timeElapsed) s.")
 }
 
-print("The \(turn - 1) number spoken is \(lastNumber)")
+printTimeElapsedWhenRunningCode(title: "") {
+    var spoken = memory[lastNumber]!
+    
+    while turn < 30000001 {
+        if spoken.1 == 0 {
+            lastNumber = 0
+        } else {
+            lastNumber = spoken.0 - spoken.1
+        }
+
+        let temp = memory[lastNumber] ?? (0, 0)
+        spoken = (turn, temp.0)
+        memory[lastNumber] = spoken
+        
+        turn += 1
+        if turn % 10000 == 0 { print("At \(turn)") }
+    }
+
+    print("The \(turn - 1) number spoken is \(lastNumber)")
+}
+
+// The 30000000 number spoken is 24065124
+// Time elapsed for : 2.087020993232727 s.
