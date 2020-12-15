b7224746f78f1fae089c2cb831858409b1d61101
Emiel Lensink <emiel.lensink@sogeti.com>
2020-12-15 10:59:39 +0100

    Slightly faster but more memory consuming solution for day 15

--- 15/15-2.playground/Contents.swift
+++ 15/15-2.playground/Contents.swift
@@ -9,13 +9,15 @@ let numbers = input
     .map { Int($0)! }
 
 var memory: [Int: (Int, Int)] = [:]
+var memory2: [(Int, Int)] = Array<(Int, Int)>(repeating: (0, 0), count: 30000000)
 
 for (index, number) in numbers.enumerated() {
     memory[number] = (index + 1, 0)
 }
 
-var turn = numbers.count + 1
-var lastNumber = numbers.last!
+for (index, number) in numbers.enumerated() {
+    memory2[number] = (index + 1, 0)
+}
 
 func printTimeElapsedWhenRunningCode(title:String, operation:()->()) {
     let startTime = CFAbsoluteTimeGetCurrent()
@@ -24,9 +26,12 @@ func printTimeElapsedWhenRunningCode(title:String, operation:()->()) {
     print("Time elapsed for \(title): \(timeElapsed) s.")
 }
 
-printTimeElapsedWhenRunningCode(title: "") {
+printTimeElapsedWhenRunningCode(title: "Dictionary") {
+    var turn = numbers.count + 1
+    var lastNumber = numbers.last!
+
     var spoken = memory[lastNumber]!
-    
+
     while turn < 30000001 {
         if spoken.1 == 0 {
             lastNumber = 0
@@ -37,9 +42,31 @@ printTimeElapsedWhenRunningCode(title: "") {
         let temp = memory[lastNumber] ?? (0, 0)
         spoken = (turn, temp.0)
         memory[lastNumber] = spoken
-        
+
         turn += 1
-        if turn % 10000 == 0 { print("At \(turn)") }
+    }
+
+    print("The \(turn - 1) number spoken is \(lastNumber)")
+}
+
+printTimeElapsedWhenRunningCode(title: "Array") {
+    var turn = numbers.count + 1
+    var lastNumber = numbers.last!
+
+    var spoken = memory2[lastNumber]
+
+    while turn < 30000001 {
+        if spoken.1 == 0 {
+            lastNumber = 0
+        } else {
+            lastNumber = spoken.0 - spoken.1
+        }
+
+        let temp = memory2[lastNumber]
+        spoken = (turn, temp.0)
+        memory2[lastNumber] = spoken
+
+        turn += 1
     }
 
     print("The \(turn - 1) number spoken is \(lastNumber)")
@@ -47,3 +74,6 @@ printTimeElapsedWhenRunningCode(title: "") {
 
 // The 30000000 number spoken is 24065124
 // Time elapsed for : 2.087020993232727 s.
+
+// The 30000000 number spoken is 24065124
+// Time elapsed for Array: 0.8168659210205078 s.
